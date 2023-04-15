87a3acbf853a02170af55b4d0b91099d95f0d5d7
Bartek34295 <bkmiecik813@gmail.com>
2023-04-15 17:21:11 +0200

    Lab 5

---
+++ LAB_5/2/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const config = require('./config').config;
+const questions = require('./questions').questions;
+const app = express();
+
+app.get('/api/questions', (_request, response) => {
+   response.send(questions);
+});
+
+app.listen(config.port, function () {
+    console.info(`Server is running at port 3000`);
+   });
